e78e28aa8caa38d5a171b4aebf2c5d00e484b66d
xiaojieluo <xiaojieluoff@gmail.com>
2020-12-10 11:56:41 +0800

    删除无用文件

--- sourcehub/controller/__init__.py
+++
@@ -1,5 +0,0 @@
-# from .IndexController import index as index_bp
-# from .TagController import tag as tag_bp
-# from .UserController import user as user_bp
-# from .LinkController import link as link_bp
-# from .WebsiteController import website as website_bp
--- sourcehub/static/js/search.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	document.write('<form class="bdsearch" id="bdsearch"  action="/plus/search.php" method="get" clear="true" target="_blank">');
-document.write('<input name="s" value="1689337918060193057" type="hidden">');
-document.write('<input type="hidden" name="nsid" value="0"> ');
-document.write('<input type="text" value="" autocomplete="off" autocorrect="off" placeholder="请输入网站名称" maxlength="64" id="bdcs-search-form-input" name="q" class="search_input">');
-document.write('<input type="submit" class="search_bt" value="" id="bdcs-search-form-submit">');
-document.write('</form>');
-
-	})();
